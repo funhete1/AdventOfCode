edf6747e903b386aab9b524367ec98f4f4c20827
funhete1 <arthurm51@gmail.com>
2021-12-20 13:11:37 +0000

    .

--- day4.py
+++ day4.py
@@ -1,12 +1,17 @@
 numbers = []
 guesses = []
-i=0
+counter = 0
 with open("input4.txt", "r") as f:
     numbers = f.readlines()
     guesses = numbers[0].split(",")
-    print(numbers[2])
-    print(numbers[3])
     numbers.pop(0)
     numbers.pop(0)
-    for i in range(0,len(numbers),5): #vai matriz a matriz
+    for i in range(len(numbers)): #vai matriz a matriz
+        if i%5==0:counter+=1
         for j in range(5): #linha da matriz
+            for e in guesses:
+                if numbers[i][j] ==  e:
+                    numbers[i][j] = '0'
+        if numbers[i] == ['0','0','0','0','0']:break
+    print(numbers)
+        
